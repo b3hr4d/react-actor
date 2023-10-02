c2f49045602e17b7df651218b4a4894a4d811ed4
b3hr4d <behradmusiclove@yahoo.com>
2023-10-02 20:06:14 +0300

    mock agent

--- packages/core/test/actor.test.ts
+++ packages/core/test/actor.test.ts
@@ -1,65 +1,72 @@
-import { Actor, Cbor, HttpAgent } from "@dfinity/agent"
+import { Cbor, HttpAgent } from "@dfinity/agent"
 import { IDL } from "@dfinity/candid"
 import { Principal } from "@dfinity/principal"
 import fetchMock from "jest-fetch-mock"
+import createICStoreAndActions from "../src"
+import { createActor } from "./candid/hello"
 
 fetchMock.enableMocks()
 
-describe("makeActor", () => {
-  const canisterDecodedReturnValue = "Hello, World!"
-  const expectedReplyArg = IDL.encode([IDL.Text], [canisterDecodedReturnValue])
+const canisterDecodedReturnValue = "Hello, World!"
+const expectedReplyArg = IDL.encode([IDL.Text], [canisterDecodedReturnValue])
 
-  fetchMock.mockResponse(async (req) => {
-    console.log("mockResponse", req.url)
-    if (req.url.endsWith("/call")) {
-      return Promise.resolve({
-        status: 202,
-        statusText: "accepted",
-      })
-    } else {
-      const responseObj = {
-        status: "replied",
-        reply: {
-          arg: expectedReplyArg,
-        },
-      }
+fetchMock.mockResponse(async (req) => {
+  console.log("mockResponse", req.url.split("/").findLast((_) => _) ?? "")
+  const responseObj = {
+    status: "replied",
+    reply: {
+      arg: expectedReplyArg,
+    },
+    rootKey: [
+      48, 129, 130, 48, 29, 6, 13, 43, 6, 1, 4, 1, 130, 220, 124, 5, 3, 1, 2, 1,
+      6, 12, 43, 6, 1, 4, 1, 130, 220, 124, 5, 3, 2, 1, 3, 97, 0, 152, 245, 4,
+      39, 146, 174, 234, 225, 244, 127, 143, 22, 30, 244, 96, 2, 185, 106, 236,
+      194, 117, 132, 255, 155, 140, 185, 7, 232, 134, 106, 159, 201, 151, 124,
+      250, 187, 134, 24, 222, 199, 14, 102, 155, 207, 175, 235, 0, 227, 1, 25,
+      5, 48, 61, 231, 72, 146, 168, 216, 60, 223, 174, 138, 180, 149, 247, 3,
+      97, 218, 226, 71, 68, 160, 168, 2, 86, 197, 60, 84, 86, 174, 6, 208, 158,
+      57, 110, 122, 8, 222, 41, 146, 247, 102, 75, 37, 145, 253,
+    ],
+  }
 
-      // Simulate encoding the response as CBOR
-      const cborData = Cbor.encode(responseObj)
-
-      return Promise.resolve({
-        status: 200,
-        statusText: "ok",
-        body: cborData,
-      })
-    }
-  })
-
-  const actorInterface = () => {
-    return IDL.Service({
-      greet: IDL.Func([IDL.Text], [IDL.Text], ["query"]),
-      greet_update: IDL.Func([IDL.Text], [IDL.Text]),
+  if (req.url.endsWith("/call")) {
+    return Promise.resolve({
+      status: 202,
+      statusText: "accepted",
     })
   }
 
+  return Promise.resolve({
+    status: 200,
+    statusText: "ok",
+    body: Cbor.encode(responseObj),
+  })
+})
+
+describe("makeActor", () => {
   const agent = new HttpAgent({
-    host: "http://localhost",
+    host: "http://f866-178-128-192-251.ngrok-free.app",
   })
 
   it("should call the actor method", async () => {
-    const canisterId = Principal.fromText("2chl6-4hpzw-vqaaa-aaaaa-c")
-    const actor = Actor.createActor(actorInterface, {
-      canisterId,
-      agent,
-    })
+    const canisterId = Principal.fromText("bd3sg-teaaa-aaaaa-qaaba-cai")
+    agent.fetchRootKey()
+    const [{}, { startActivation, callActorMethod }] = createICStoreAndActions(
+      () =>
+        createActor(canisterId, {
+          agent,
+        })
+    )
+    // subscribe(console.log)
 
-    const reply = await actor.greet("test")
+    startActivation()
+
+    const reply = await callActorMethod("greet", "World")
 
     expect(reply).toEqual(canisterDecodedReturnValue)
 
-    // const replyUpdate = await actor.greet_update("test")
-    // console.log("update-reply", replyUpdate)
+    const replyUpdate = await callActorMethod("greet_update", "World")
 
-    // expect(replyUpdate).toEqual(canisterDecodedReturnValue)
+    expect(replyUpdate).toEqual(canisterDecodedReturnValue)
   })
 })
